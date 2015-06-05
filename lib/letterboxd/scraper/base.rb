0bce2f50c8b52150be056e0ccf37870be069ada9
Koen Romers <info@koenromers.com>
2015-06-05 19:22:05 +0200

    fix(emoji): strip

--- lib/letterboxd/scraper/base.rb
+++ lib/letterboxd/scraper/base.rb
@@ -94,10 +94,36 @@ module Letterboxd
 
         list = doc.css('.person-table .table-person h3 > a')
         list.each do |node|
-          items << {name: node.text.to_s.squish, username: strip_slug(node.attribute('href').value)}
+          items << {name: strip_emoji(node.text.to_s.squish), username: strip_slug(node.attribute('href').value)}
         end
         items.flatten
       end
+
+      protected
+        def strip_emoji ( str )
+          str = str.force_encoding('utf-8').encode
+          clean_text = ""
+
+          # emoticons  1F601 - 1F64F
+          regex = /[\u{1f600}-\u{1f64f}]/
+          clean_text = str.gsub regex, ''
+
+          #dingbats 2702 - 27B0
+          regex = /[\u{2702}-\u{27b0}]/
+          clean_text = clean_text.gsub regex, ''
+
+          # transport/map symbols
+          regex = /[\u{1f680}-\u{1f6ff}]/
+          clean_text = clean_text.gsub regex, ''
+
+          # enclosed chars  24C2 - 1F251
+          regex = /[\u{24C2}-\u{1F251}]/
+          clean_text = clean_text.gsub regex, ''
+
+          # symbols & pics
+          regex = /[\u{1f300}-\u{1f5ff}]/
+          clean_text = clean_text.gsub regex, ''
+        end
 
     end
   end
