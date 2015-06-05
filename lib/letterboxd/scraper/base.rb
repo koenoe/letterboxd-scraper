d9614b2d6ce166f20eaad234b172feff722d1379
Koen Romers <info@koenromers.com>
2015-06-05 13:23:16 +0200

    feat(sleep): add sleep

--- lib/letterboxd/scraper/base.rb
+++ lib/letterboxd/scraper/base.rb
@@ -12,6 +12,7 @@ module Letterboxd
       def fetch(url)
         puts "Fetching #{url} ..."
         Nokogiri::HTML(open(base_url + url, "Host" => "letterboxd.com" ))
+        sleep(0.25)
       end
 
       def fetch_films(url, number_of_pages_limit = nil)
